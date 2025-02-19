6e8db157bafcc80e9393c685494ab023c054df70
Francois Beetge <7982855+dampbacon@users.noreply.github.com>
2025-02-19 19:13:45 +1100

    Add project files.

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Drawing.Text
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    <DllImport("gdi32.dll")>
+    Private Shared Function AddFontMemResourceEx(pbFont As IntPtr, cbFont As UInteger,
+                                                 pdv As IntPtr, ByRef pcFonts As UInteger) As IntPtr
+    End Function
+
+    Private fonts As New PrivateFontCollection()
+    Private myFont As Font
+
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+End Class
